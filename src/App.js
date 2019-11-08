0c6b0645d58509435ea832e5be132a666590a628
Kamel Bouali <dvbouali@gmail.com>
2019-11-08 18:52:50 +0100

    first apparition of routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React, {Component} from 'react';
+import {BrowserRouter as  Router, Route, Switch} from 'react-router-dom';
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import ErrorPage from "./components/ErrorPage";
+
 
 
  class App extends Component {
    render(){
     return (
-      <React.Fragment>  </React.Fragment>
+      <React.Fragment>
+        <Router>
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route  path='/about' component={About} />
+            <Route  path='/contact' component={Contact} />
+            <Route component={ErrorPage} />
+          </Switch>
+        </Router>
+      </React.Fragment>
       );
    }
   
